6c30868e0906fb51c9f3191917030178de3e48f7
emrahsusam <emrahsusam01@gmail.com>
2022-03-04 04:22:31 +0300

    Sekmeler güncellendi

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Badescarf - Şal & Eşarp | Home' });
 });
 
+router.get('/sal', function(req, res, next) {
+  res.render('sal', { title: 'Badescarf - Şal & Eşarp | Home' });
+});
+
+router.get('/esarp', function(req, res, next) {
+  res.render('esarp', { title: 'Badescarf - Şal & Eşarp | Home' });
+});
+
+router.get('/hediyekutu', function(req, res, next) {
+  res.render('hediyekutu', { title: 'Badescarf - Şal & Eşarp | Home' });
+});
+
+router.get('/aksesuar', function(req, res, next) {
+  res.render('aksesuar', { title: 'Badescarf - Şal & Eşarp | Home' });
+});
+
 module.exports = router;

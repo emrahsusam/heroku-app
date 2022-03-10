86956f647a33d1ba2a94c999e1687317c27bbfe1
emrahsusam <94632756+emrahsusam@users.noreply.github.com>
2022-03-10 17:17:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,26 @@ const createError       =require('http-errors'),
       logger            = require('morgan'),
       indexRouter       = require('./routes/index'),
       usersRouter       = require('./routes/users'),
+      User              =require('./models/userModel'),
       app = express();
 
 // view engine setup
-mongoose.connect('mongodb://heroku/badescarf');
+mongoose.connect('mongodb+srv://emrahsusam:eS197644@heroku-app.tqour.mongodb.net/heroku-app?retryWrites=true&w=majority');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Passport Config
+app.use(require('express-session')({
+  secret:'badescarf',
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(logger('dev'));
 app.use(express.json());
